e901b97203895f07a377cea211e9d53a4807054d
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-04-03 15:35:04 +0530

    feat: basic folder stucture and style configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Work from './components/Work';
+import Skills from './components/Skills';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-red-300 font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
+      <Hero />
+      <Work />
+      <Skills />
+      <About />
+      <Contact />
+      <Footer />
     </>
   );
 }
