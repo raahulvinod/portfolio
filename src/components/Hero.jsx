59708a3ba2572402c7b1f30b3e7c3b6f80ba1136
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-04-05 19:27:54 +0530

    feat: hero basic structure created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,32 @@
+import heroImage from '../assets/profilepic.png';
+import notebookL from '../assets/notebookL.png';
+import notebookM from '../assets/notebookM.png';
+import mobileR from '../assets/mobileR.png';
+import { TypeAnimation } from 'react-type-animation';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <img src={notebookL} alt="" />
+      <img src={notebookM} alt="" />
+      <img src={mobileR} alt="" />
+
+      <div>
+        <h1>
+          <span>
+            <p>I'm a </p>
+            <br />
+            <TypeAnimation></TypeAnimation>
+          </span>
+        </h1>
+        <p></p>
+      </div>
+
+      <div>
+        <img src={heroImage} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
