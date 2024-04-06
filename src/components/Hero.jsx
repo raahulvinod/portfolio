114a0d2ee2f9d5d9080744e7668a25af7b646e58
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-04-06 13:46:10 +0530

    feat: add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,23 +6,46 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
   return (
-    <div>
-      <img src={notebookL} alt="" />
-      <img src={notebookM} alt="" />
-      <img src={mobileR} alt="" />
+    <div className="relative grid sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[70vh] mx-auto py-8">
+      <img
+        className="absolute z-[-1] w-[400px] bottom-0 left-[-170px]"
+        src={notebookL}
+        alt=""
+      />
+      <img
+        className="absolute z-[-1] w-[400px] bottom-0 left-[320px]"
+        src={notebookM}
+        alt=""
+      />
+      <img
+        className="absolute z-[-1] w-[200px] bottom-[500px] left-[450px]"
+        src={mobileR}
+        alt=""
+      />
 
-      <div>
-        <h1>
-          <span>
-            <p>I'm a </p>
-            <br />
-            <TypeAnimation></TypeAnimation>
-          </span>
+      <div className="glass px-5 col-span-2 my-auto">
+        <h1 className="text-white text-4xl sm:text-5xl lg:text-8xl font-extrabold">
+          <p className="mb-4 text-gray-500">I'm a </p>
+
+          <TypeAnimation
+            sequence={[
+              'Full Stack Dev',
+              1000,
+              'Web Designer',
+              1000,
+              'Web Developer',
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          ></TypeAnimation>
         </h1>
-        <p></p>
+        <p className="font-bold text-gray-300 my-6 sm:text-lg lg:text-xl max-w-[200px] md:max-w-[600px] ml-1">
+          My name is rahul and i have 2+ years experience in web development.
+        </p>
       </div>
 
-      <div>
+      <div className="absolute bottom-0 right-0 w-[300px] lg:w-[500px]">
         <img src={heroImage} alt="" />
       </div>
     </div>
