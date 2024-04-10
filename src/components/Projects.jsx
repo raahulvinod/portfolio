354df3c6404bd650493830fa3015a64d582c12a1
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-04-10 19:38:53 +0530

    feat: add project component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,50 @@
+const projects = [
+  { id: 1, img: proj1, title: 'E-commerce Website', liveUrl: '/', github: '/' },
+  { id: 2, img: proj2, title: 'Blog Platform', liveUrl: '/', github: '/' },
+  { id: 3, img: proj3, title: 'Chat Application', liveUrl: '/', github: '/' },
+  { id: 4, img: proj4, title: 'Portfolio Website', liveUrl: '/', github: '/' },
+  { id: 5, img: proj5, title: 'Social Media App', liveUrl: '/', github: '/' },
+  { id: 6, img: proj6, title: 'Online Learning Platform', liveUrl: '/' },
+];
+
+const Projects = () => {
+  return (
+    <div
+      className="max-w-[1200px] mx-auto grid grid-cols-2 gap-4 glass p-6"
+      id="portfolio"
+    >
+      <div className="col-span-2 my-4">
+        <h1 className="text-4xl mb-4 text-gray-300">My Projects</h1>
+      </div>
+      {projects.map((project) => (
+        <div
+          key={project.id}
+          className="glass transform transition-transform hover:scale-105 duration-300 overflow-hidden shadow-lg md:h-[200px] group relative"
+        >
+          <img src={project.img} alt="" />
+          <div className="group-hover:opacity-90 opacity-0 bg-[#232323] absolute transition-opacity duration-300 inset-0 flex flex-col justify-center items-center">
+            <span className="text-sm md:text-2xl font-bold text-white mb-4">
+              {project.title}
+            </span>
+            <div className="flex gap-1">
+              <a
+                href={project.liveUrl}
+                className="bg-white px-5 py-2 rounded-xl font-bold"
+              >
+                Github
+              </a>
+              <a
+                href={project.github}
+                className="bg-white px-5 py-2 rounded-xl font-bold"
+              >
+                Live
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
